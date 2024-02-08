903e16ccb616820592bff1d9265803d68c7300cc
WanLingLin595 <153817474+WanLingLin595@users.noreply.github.com>
2024-02-08 17:32:31 +0800

    Add files via upload

---
+++ Random_model_wire.py
@@ -0,0 +1,120 @@
+import networkx as nx
+import numpy as np
+from BCube import BCube_n_k
+import random
+import xlwt
+import time
+from multiprocessing import Pool
+from Performance_analysis_wires import route_path
+
+
+def test_core(n, k, failure_s):
+    edges = BCube_n_k(n, k).wire
+    servers = BCube_n_k(n, k).server
+
+    LBC = nx.Graph()
+    for i in range(len(edges)):
+        LBC.add_edges_from(edges[i])
+
+    # Choose the faulty wires
+    failure_l = 15   # The number of faulty wires increasing each time
+    APL = []
+    ABT = []
+    RFR = []
+    abt, apl, tfr = route_path(LBC, servers, edges)
+    ABT.append(abt)
+    APL.append(apl)
+    RFR.append(tfr)
+    failre_num = 0
+    while failre_num < failure_s:
+        choose_F = []
+        for i in range(len(edges)):
+            choose_F.extend(edges[i])
+        F = random.sample(choose_F, failure_l)
+        failre_num += failure_l
+
+        LBC.remove_edges_from(F)
+        for i in range(len(edges)):
+            edges[i] = list(set(edges[i]) - set(F))
+
+        abt, apl, tfr = route_path(LBC, servers, edges)
+        ABT.append(abt)
+        APL.append(apl)
+        RFR.append(tfr)
+
+    return ABT, APL, RFR
+
+
+def pool_core(n, k, failure_s, processes_num):
+    pool = Pool(processes=processes_num)
+    pool_set = []
+    for i in range(processes_num):
+        pool_i = pool.apply_async(test_core, (n, k, failure_s))
+        pool_set.append(pool_i)
+    pool.close()
+    pool.join()
+    APL_set = []
+    ABT_set = []
+    RFR_set = []
+    for pool_i in pool_set:
+        x, y, z = pool_i.get()
+        ABT_set.append(x)
+        APL_set.append(y)
+        RFR_set.append(z)
+
+    # record all data
+    s1 = "BC({0},{1},{2})_Random.xls".format(n, k, failure_s)
+    bcube = xlwt.Workbook(encoding='utf-8')
+    sheet1 = bcube.add_sheet('bcube', cell_overwrite_ok=True)
+    sheet1.write(0, 0, 'F')
+    sheet1.write(0, 1, 'ABT-avg')
+    sheet1.write(0, 2, 'APL-avg')
+    sheet1.write(0, 3, 'RFR-avg')
+
+    # Calculation of the average of the data
+    ABT_AVG = []
+    APL_AVG = []
+    RFR_AVG = []
+    for i in range(len(ABT_set[0])):
+        ABT_i = []
+        APL_i = []
+        RFR_i = []
+        for j in range(len(ABT_set)):
+            ABT_i.append(ABT_set[j][i])
+            APL_i.append(APL_set[j][i])
+            RFR_i.append(RFR_set[j][i])
+        tx1 = np.array(ABT_i)
+        tx2 = np.array(APL_i)
+        tx3 = np.array(RFR_i)
+        avg1 = np.average(tx1)
+        avg2 = np.average(tx2)
+        avg3 = np.average(tx3)
+        ABT_AVG.append(avg1)
+        APL_AVG.append(avg2)
+        RFR_AVG.append(avg3)
+
+    # record data
+    for i in range(1, len(ABT_AVG) + 1):
+        sheet1.write(i, 1, ABT_AVG[i - 1])
+        sheet1.write(i, 2, APL_AVG[i - 1])
+        sheet1.write(i, 3, RFR_AVG[i - 1])
+
+    # all data
+    # for i in range(len(ABT_set)):
+    #     for j in range(1, len(ABT_set[i]) + 1):
+    #         sheet1.write(j, i+4, ABT_set[i][j - 1])
+    #         sheet1.write(j, i + 4 + len(ABT_set), APL_set[i][j - 1])
+    #         sheet1.write(j, i + 4 + 2 * len(ABT_set), TFR_set[i][j - 1])
+
+    bcube.save(s1)
+
+
+if __name__ == '__main__':
+    n = 8
+    k = 2
+    f_l = 538
+
+    time_start = time.time()
+    pool_core(n, k, f_l, 100)
+    time_end = time.time()
+    print('BCube(8,2)_random:', time_end - time_start)
